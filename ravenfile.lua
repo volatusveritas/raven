1b5571747dca6d6bb1f225c984e9ff50946290a9
Volatus <volatusCoder@outlook.com>
2024-05-25 22:05:24 -0300

    Update the ravenfile to use new features

--- ravenfile.lua
+++ ravenfile.lua
@@ -1,9 +1,17 @@
 local PROGRAM_NAME = 'raven'
 local BUILD_PATH = 'build'
 
-run(string.format(
-    "odin build %s -out:%s/%s.exe",
+runf(
+    "odin build %s -out:%s/%s_test.exe",
     PROGRAM_NAME,
     BUILD_PATH,
     PROGRAM_NAME
-))
+)
+
+function cmd.testfunc(args)
+    print("Test function called with")
+
+    for _, arg in ipairs(args) do
+        print(arg)
+    end
+end
