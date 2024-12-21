751bb61c4973f8794a81a72034bb68bd6ea680e7
Volatus <volatusCoder@outlook.com>
2024-12-21 20:35:33 -0300

    Add check command to ravenfile

--- ravenfile.lua
+++ ravenfile.lua
@@ -9,3 +9,7 @@ end
 function raven.commands.debug()
     raven.run 'odin build raven -out:build/raven_db.exe -o:none -debug'
 end
+
+function raven.commands.check()
+    raven.run 'odin check raven -vet-cast -vet-semicolon -vet-shadowing -vet-style -vet-unused -vet-unused-imports -vet-unused-variables -vet-using-param -vet-using-stmt -warnings-as-errors -strict-style'
+end
