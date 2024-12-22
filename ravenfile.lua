1f833d75691efca743f8b1a7216c91ba3cf2fe13
Volatus <volatusCoder@outlook.com>
2024-12-22 15:52:52 -0300

    Create build folder if it doesn't exist

--- ravenfile.lua
+++ ravenfile.lua
@@ -1,3 +1,13 @@
+if not raven.exists('build', raven.EXISTS_FILTER_DIRECTORY) then
+    local build_dir_err = raven.create_directory('build')
+
+    if build_dir_err ~= raven.ERROR_NONE then
+        error('Failed to create directory "build/"')
+    end
+
+    print('Created directory "build/"')
+end
+
 function raven.commands.release()
     raven.run 'odin build raven -out:build/raven_release.exe -o:speed -disable-assert -no-bounds-check'
 end
